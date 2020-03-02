ff3d5b97b45cd038121ef069b40505028544d2eb
Martín Moreno <martin.m.moreno@icloud.com>
2020-03-02 08:29:27 -0800

    Clean up for the task.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
 import React, {useState} from 'react';
-import './App.css';
+import './App.scss';
 
-const BusinessSolverButton = (props) => {
-  const { length } = props;
+/**
+ *
+ * Use of concat for arrays in React
+ * source: https://medium.com/javascript-in-plain-english/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
+ *
+ */
 
-  return (
-    <button
-      className="busSolverButton"
-      type="button"
-    >
-      Button # {length + 1}
-    </button>
-  );
-};
+function App() {
+  const [countLog, setCountLog] = useState(0);
 
-const BusinessSolverForm = () => {
   const [inputList, setInputList] = useState([]);
 
-  const addAnotherButton = (event) => {
-    setInputList(inputList.concat(<BusinessSolverButton key={inputList.length} length={inputList.length} />));
+  const BusinessSolverButton = (props) => {
+    const { buttonNumber } = props;
+
+    const [clickCount, setClickCount] = useState(0);
+
+    return (
+      <div>
+        <button
+          className="busSolverButton"
+          onClick={() => setClickCount(clickCount + 1)}
+          type="button"
+        >
+          {`Button: ${buttonNumber} Clicks: ${clickCount}`}
+        </button>
+      </div>
+    );
   };
+
+  const addAnotherButton = () => {
+    setCountLog(countLog + 1);
+
+    setInputList(inputList.concat(
+      <BusinessSolverButton
+        buttonNumber={countLog}
+        numClicks={0}
+      />
+    ));
+  };
+
+  const handleDeleteButton = (idx) => {
+    const updatedList = inputList.filter((elem, index) => idx !== index);
+
+    setInputList(updatedList);
+  }
 
   return (
     <div>
-      <button onClick={addAnotherButton}>Add a button</button>
-      <div>
-        {inputList}
+      <button
+        onClick={addAnotherButton}
+      >
+        Add a button
+      </button>
+      <div
+        className="container"
+      >
+        {inputList.map((buttonElement, idx) => (
+          <div
+            key={idx}
+            className="locaseWrapper"
+          >
+            {buttonElement}
+            <button
+              className="busSolverButton deleteButton"
+              onClick={() => handleDeleteButton(idx)}
+              type="button"
+            >
+              {`DELETE BUTTON ${buttonElement.props.buttonNumber}`}
+            </button>
+          </div>
+        )
+        )}
       </div>
     </div>
   );
-};
-
-function App() {
-  return (
-    <BusinessSolverForm />
-  )
 }
 
 export default App;
